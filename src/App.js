c4e4da9cea27c24e3efe9a3a7b39d34a54d5628d
bofletcher <hfletche.hf@gmail.com>
2020-07-17 13:58:08 -0400

    Added components folder Added Menu component Added Menu module css file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
+import Menu from './components/Menu/Menu';
+
 
 function App() {
   return (
     <div className="App">
+      <Menu />
       BOFLETCHER.COM
     </div>
   );
---
+++ src/components/Menu/Menu.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from './Menu.module.css'
+
+const Menu = () => (
+  <div className={styles.startMenu}>
+    Menu
+  </div>
+)
+
+export default Menu;
