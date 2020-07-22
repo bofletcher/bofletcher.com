cc51175fb34de3f2669403bc2c818c789582d6b7
bofletcher <hfletche.hf@gmail.com>
2020-07-22 16:02:11 -0400

    Built out a Project page.

--- src/components/Pages/Contact/Contact.jsx
+++ src/components/Pages/Contact/Contact.jsx
@@ -9,7 +9,7 @@ function Contact() {
           <a href="sms:+16787895957&body=Hi%2C%20I%20would%20like%20to%20speak%20with%20you%20about%20Web%20Development%2FSoftware%20Development%20opportunities.%20">Text: (678) 789-5957 (click me)</a>
         </div>
         <div>
-          <a href="mailto:hfletche.hf@gmail.com">Email: hfletche.hf@gmail.com (click me)</a>
+          <a href="mailto: hfletche.hf@gmail.com">Email: hfletche.hf@gmail.com (click me)</a>
         </div>
       </div>
     </div>
--- src/components/Pages/Projects/Projects.jsx
+++ src/components/Pages/Projects/Projects.jsx
@@ -5,7 +5,21 @@ import styles from "./Projects.module.css"
 function Projects() {
   return (
     <div className={styles.projectPage}>
-        PROJECTS
+      <div className={styles.projectPageBody}>
+        <div className={styles.projectContainer}>
+          Project Name:
+          <a href="//hexproof.club" target="_blank" rel="noopener noreferrer" >HEXPROOF.CLUB</a>
+          Project Description:
+          <p>A React App for a fictional band called hexproof. The app uses a custom audio player and a custom image carousel!</p>
+        </div>
+
+        <div className={styles.projectContainer}>
+          Project Name:
+          <a href="//shredder-29fbd.firebaseapp.com" target="_blank" rel="noopener noreferrer" >SHREDDER</a>
+          Project Description: 
+          <p>A React app for Tracking your weight training using Firebase as a backend. Work in Progress</p>
+        </div>
+      </div>
     </div>
   )
 }
