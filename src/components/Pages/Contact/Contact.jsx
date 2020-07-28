119f96d194f91c6e2f0dbbdb8047f7a851eacfc6
bofletcher <hfletche.hf@gmail.com>
2020-07-28 19:33:43 -0400

    Updated pages

--- src/components/Pages/Contact/Contact.jsx
+++ src/components/Pages/Contact/Contact.jsx
@@ -5,11 +5,11 @@ function Contact() {
   return (
     <div className={styles.contactPage}>
       <div className={styles.contactPageBody}>
-        <div>
-          <a href="sms:+16787895957&body=Hi%2C%20I%20would%20like%20to%20speak%20with%20you%20about%20Web%20Development%2FSoftware%20Development%20opportunities.%20">Text: (678) 789-5957 (click me)</a>
+        <div className={styles.contactLinkWrapper}>
+          <a className={styles.contactLink} href="sms:+16787895957&body=Hi%2C%20I%20would%20like%20to%20speak%20with%20you%20about%20Web%20Development%2FSoftware%20Development%20opportunities.%20">Text: (678) 789-5957 (click me)</a>
         </div>
-        <div>
-          <a href="mailto: hfletche.hf@gmail.com">Email: hfletche.hf@gmail.com (click me)</a>
+        <div className={styles.contactLinkWrapper}>
+          <a className={styles.contactLink} href="mailto: hfletche.hf@gmail.com">Email: hfletche.hf@gmail.com</a>
         </div>
       </div>
     </div>
--- src/components/Pages/Projects/Projects.jsx
+++ src/components/Pages/Projects/Projects.jsx
@@ -11,7 +11,7 @@ function Projects() {
     <div className={styles.projectPage}>
       <div className={styles.projectPageBody}>
         <div className={styles.projectContainer}>
-          <a href="//hexproof.club" target="_blank" rel="noopener noreferrer" >
+          <a className={styles.projectA} href="//hexproof.club" target="_blank" rel="noopener noreferrer" >
           <img className={styles.projectIconImage} src={globeIcon} alt="windows-globe-icon"/>
            <div> HEXPROOF.CLUB </div>
            </a>
@@ -20,7 +20,7 @@ function Projects() {
         </div>
 
         <div className={styles.projectContainer}>
-          <a href="//shredder-29fbd.firebaseapp.com" target="_blank" rel="noopener noreferrer" >
+          <a className={styles.projectA} href="//shredder-29fbd.firebaseapp.com" target="_blank" rel="noopener noreferrer" >
           <img className={styles.projectIconImage} src={joystickIcon} alt="windows-globe-icon"/>
             SHREDDER</a>
           {/* Project Description: 
